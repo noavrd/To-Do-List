075bfbf82e1f1e457e3e93383839bdf354678597
noavrd <77359733+noavrd@users.noreply.github.com>
2021-01-30 02:55:59 +0200

    add delete and checkbox

--- script.js
+++ script.js
@@ -5,30 +5,84 @@ let button = document.getElementById("add-button");
 let counter = document.getElementById("counter");
 let countTask = 0;
 let sort = document.getElementById("sort-button");
+let option = document.getElementsByTagName("option");
+let arrInArr = [];
+let arr = [];
+let deleteAll = document.getElementById("Delete all");
+let listText = document.createElement("div");
 
 
-//enter a task to the list
+
+
+//add a task to the list
 button.onclick = () => {
-    let listText = document.createElement("div");
+    if (input.value === "") { //check that the input is not empty
+        alert("Add a task");
+        return;
+    }
+    listText = document.createElement("div");
     listText.className = "todo-container";
     viewSection.appendChild(listText);
+    deleteTask(listText);
+    addSelect(listText);
+    addValue(listText);
 
-    //enter priority
+    addPriority(listText);
+    addDate(listText);
+    countIncrease();
+    arr.push(listText);
+
+};
+sort.onclick = () => {
+    for (let i = 1; i < arrInArr.length; i++) {
+        if(i % 5 === 0) {
+            for (let j = 1; j < option.length ; j++) {
+                if (i === option[j]) {
+                    arr.push(i-1, i, i+1,i+2,i+3);
+                }
+            }
+        }
+    }
+    console.log(arr);
+    viewSection.append(arr);
+
+    
+}
+
+
+function addSelect(listText) {
+    let checkBox = document.createElement("input");
+    checkBox.type = "checkBox";
+    checkBox.className = "check";
+    listText.appendChild(checkBox);
+}
+
+
+
+//delete task
+function deleteTask(listText) {
+    let deleteButton = document.createElement("button");
+    listText.appendChild(deleteButton);
+    deleteButton.className = "delete";
+    deleteButton.textContent = "delete";
+    deleteButton.onclick = function () {
+        viewSection.removeChild(listText);
+        countTask--;
+        counter.textContent = countTask;
+    };
+}
+
+//add priority
+function addPriority(listText) {
     let selectValue = document.createElement("div");
     selectValue.textContent = select.value;
     selectValue.className = "todo-priority";
     select.value = 1;
     listText.appendChild(selectValue);
+}
 
-    //enter date
-    let timeValue = document.createElement("div");
-    let time = new Date();
-    time = time.toISOString().split('T')[0] + " " + time.toTimeString().split(" ")[0];
-    timeValue.textContent = time;
-    timeValue.className = "todo-created-at";
-    listText.appendChild(timeValue);
-
-    //enter value
+//add value
+function addValue(listText) {
     let inputValue = document.createElement("div");
     inputValue.textContent = input.value;
     inputValue.className = "todo-text";
@@ -36,12 +90,24 @@ button.onclick = () => {
     input.focus();
     listText.appendChild(inputValue);
 
-    //add count
+}
+
+//add date
+function addDate(listText) {
+    let timeValue = document.createElement("div");
+    let time = new Date();
+    time = time.toISOString().split('T')[0] + " " + time.toTimeString().split(" ")[0];
+    timeValue.textContent = time;
+    timeValue.className = "todo-created-at";
+    listText.appendChild(timeValue);
+    
+
+
+}
+
+//add count
+function countIncrease() {
     countTask++;
     counter.textContent = countTask;
-
 }
 
-button.onclick = () => {
-
-}
